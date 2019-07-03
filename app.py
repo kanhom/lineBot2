fa27ef93e63e30928132480615c674018df03561
kanhom <51397256+kanhom@users.noreply.github.com>
2019-07-03 14:23:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-     if (userText== 'สวัสดี') :
+    if (userText== 'สวัสดี') :
         sendText(user,'ว่างายยยย')
     elif (userText == 'ทำไรอยู่') :
         sendText(user,'นอนเล่นนนนน')
